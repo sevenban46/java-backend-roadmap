717c156b081fc3b40b5a980e2c9ed372e9c3169e
sevenban46 <oscarcanizales2000@gmail.com>
2026-04-03 15:28:43 -0600

    mejorando la logica del cajero y aumentando mas validaciones

--- ejercicios-java/src/main/java/com/alexc/ejercicios/week01/Cajero.java
+++ ejercicios-java/src/main/java/com/alexc/ejercicios/week01/Cajero.java
@@ -5,18 +5,34 @@ import java.util.Scanner;
 public class Cajero {
     public static void main(String[] args) {
         final int saldoInicial = 1000;
+        final int montoMaximo = 500;
+        final int montoMinimo = 5;
+        final String[] billeteDisponible = { "$5", "$10", "$20" };
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Bienvenido al cajero automático");
+        System.out.println("Los billetes disponibles son: " + String.join(", ", billeteDisponible));
         System.out.println("Su saldo inicial es: " + saldoInicial);
         System.out.print("Ingrese el monto a retirar: ");
         int montoRetirar = scanner.nextInt();
-        if (montoRetirar > saldoInicial) {
-            System.out.println("Fondos insuficientes. No se puede realizar el retiro.");
-        } else {
-            int saldoFinal = saldoInicial - montoRetirar;
-            System.out.println("Retiro exitoso. Su nuevo saldo es: " + saldoFinal);
+
+        while (montoRetirar < montoMinimo || montoRetirar > saldoInicial || montoRetirar > montoMaximo
+                || montoRetirar % 5 != 0) {
+            if (montoRetirar > saldoInicial) {
+                System.out.println("Error: Fondos insuficientes. Su saldo es " + saldoInicial);
+            } else if (montoRetirar > montoMaximo) {
+                System.out.println("Error: El monto máximo es " + montoMaximo);
+            } else if (montoRetirar < montoMinimo) {
+                System.out.println("Error: El monto mínimo es " + montoMinimo);
+            } else if (montoRetirar % 5 != 0) {
+                System.out.println("Error: El monto debe ser múltiplo de 5.");
+            }
+            System.out.print("Ingrese un monto válido: ");
+            montoRetirar = scanner.nextInt();
         }
+        int saldoFinal = saldoInicial - montoRetirar;
+        System.out.println("Retiro exitoso. Su nuevo saldo es: " + saldoFinal);
+
         scanner.close();
     }
 }
