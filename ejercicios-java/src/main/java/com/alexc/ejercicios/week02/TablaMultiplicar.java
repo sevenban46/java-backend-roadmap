cd2c1719b671b612dacf9274435607f13769eb6f
sevenban46 <oscarcanizales2000@gmail.com>
2026-04-04 01:09:47 -0600

    tabla de multiplicar con for

--- ejercicios-java/src/main/java/com/alexc/ejercicios/week02/TablaMultiplicar.java
+++ ejercicios-java/src/main/java/com/alexc/ejercicios/week02/TablaMultiplicar.java
@@ -1,8 +1,20 @@
 package com.alexc.ejercicios.week02;
 
+import java.util.Scanner;
 public class TablaMultiplicar {
 
     public static void main(String[] args) {
         // TODO: pedir un numero e imprimir su tabla de multiplicar.
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un número: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Tabla de multiplicar del " + numero + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+
+        scanner.close();
     }
 }
