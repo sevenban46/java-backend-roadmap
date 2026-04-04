0f8e4a817db1ec018b14d943805770774855c767
sevenban46 <oscarcanizales2000@gmail.com>
2026-04-04 01:56:10 -0600

    cuenta bancaria

--- ejercicios-java/src/main/java/com/alexc/ejercicios/week03/CuentaBancaria.java
+++ ejercicios-java/src/main/java/com/alexc/ejercicios/week03/CuentaBancaria.java
@@ -3,5 +3,29 @@ package com.alexc.ejercicios.week03;
 public class CuentaBancaria {
 
     // TODO: agregar atributos privados como titular y saldo.
+    private String titular;
+    private double saldo;
+
     // TODO: crear metodos depositar, retirar y mostrarSaldo.
+    public CuentaBancaria(String titular, double saldo) {
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public void depositar(double cantidad) {
+        this.saldo += cantidad;
+    }
+
+    public void retirar(double cantidad) {
+        if (cantidad <= this.saldo) {
+            this.saldo -= cantidad;
+        } else {
+            System.out.println("Fondos insuficientes.");
+        }
+    }
+
+    public void mostrarSaldo() {
+        System.out.println("Saldo de " + this.titular + ": $" + this.saldo);
+    }
+
 }
