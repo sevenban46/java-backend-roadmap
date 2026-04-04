3268b8406e6fdc8cfa272a3854337f7840c84af6
sevenban46 <oscarcanizales2000@gmail.com>
2026-04-04 01:54:09 -0600

    realizando la clase poo producto

--- ejercicios-java/src/main/java/com/alexc/ejercicios/week03/Producto.java
+++ ejercicios-java/src/main/java/com/alexc/ejercicios/week03/Producto.java
@@ -1,7 +1,46 @@
 package com.alexc.ejercicios.week03;
 
 public class Producto {
-
     // TODO: agregar atributos privados: nombre, precio y stock.
+    private String nombre;
+    private double precio;
+    private int stock;
+
     // TODO: crear constructor, getters, setters y un metodo para actualizar stock.
+    public Producto(String nombre, double precio, int stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    // Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    // Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    // Método para actualizar el stock
+    public void actualizarStock(int cantidad) {
+        this.stock += cantidad;
+    }
 }
