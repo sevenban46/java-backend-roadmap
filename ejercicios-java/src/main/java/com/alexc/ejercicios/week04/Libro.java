b9c9bde570395a9d8c5d1116e8cba436ef84e89b
sevenban46 <oscarcanizales2000@gmail.com>
2026-04-06 00:53:53 -0600

    libro

--- ejercicios-java/src/main/java/com/alexc/ejercicios/week04/Libro.java
+++ ejercicios-java/src/main/java/com/alexc/ejercicios/week04/Libro.java
@@ -3,4 +3,34 @@ package com.alexc.ejercicios.week04;
 public class Libro {
 
     // TODO: agregar atributos como titulo, autor y disponibilidad.
+    private String titulo;
+    private String autor;
+    private boolean disponible;
+
+    public Libro(String titulo, String autor, boolean disponible) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponible = disponible;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+    
+    public void prestar() {
+        if (disponible) {
+            disponible = false;
+            System.out.println("Has prestado el libro: " + titulo);
+        } else {
+            System.out.println("El libro " + titulo + " no está disponible.");
+        }
+    }
 }
