2f5989300ed5a8e822971085a6248dc8a6792f05
sevenban46 <oscarcanizales2000@gmail.com>
2026-04-06 01:14:32 -0600

    hashmap

--- ejercicios-java/src/main/java/com/alexc/ejercicios/week05/InventarioHashMap.java
+++ ejercicios-java/src/main/java/com/alexc/ejercicios/week05/InventarioHashMap.java
@@ -1,8 +1,18 @@
 package com.alexc.ejercicios.week05;
 
+import java.util.HashMap;
 public class InventarioHashMap {
 
     public static void main(String[] args) {
         // TODO: usar HashMap para relacionar producto con cantidad disponible.
+        HashMap<String, Integer> inventario = new HashMap<>();
+        inventario.put("Manzanas", 50);
+        inventario.put("Naranjas", 30);
+        inventario.put("Plátanos", 20);
+        // Imprimir el inventario
+        System.out.println("Inventario:");
+        for (String producto : inventario.keySet()) {
+            System.out.println(producto + ": " + inventario.get(producto) + " unidades");
+        }
     }
 }
