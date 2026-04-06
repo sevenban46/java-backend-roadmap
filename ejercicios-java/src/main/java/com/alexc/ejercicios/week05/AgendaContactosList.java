c2c179c7ff01cc17e4b32036f6c8a5c88d6adadd
sevenban46 <oscarcanizales2000@gmail.com>
2026-04-06 01:11:22 -0600

    semana 5 arreglo de contacto

--- ejercicios-java/src/main/java/com/alexc/ejercicios/week05/AgendaContactosList.java
+++ ejercicios-java/src/main/java/com/alexc/ejercicios/week05/AgendaContactosList.java
@@ -1,8 +1,50 @@
 package com.alexc.ejercicios.week05;
 
+import java.util.ArrayList;
 public class AgendaContactosList {
 
     public static void main(String[] args) {
         // TODO: usar ArrayList para guardar, mostrar y buscar contactos.
+        ArrayList<Contacto> contactos = new ArrayList<>();
+        contactos.add(new Contacto("Alice", "123456789"));
+        contactos.add(new Contacto("Bob", "987654321"));
+        // Mostrar contactos
+        for (Contacto contacto : contactos) {
+            System.out.println(contacto);
+        }
+        // Buscar contacto por nombre
+        String nombreBuscado = "Alice";
+        for (Contacto contacto : contactos) {
+            if (contacto.getNombre().equals(nombreBuscado)) {
+                System.out.println("Contacto encontrado: " + contacto);
+                break;  
+            }
+        }
+    }
+}
+
+class Contacto {
+    private String nombre;
+    private String telefono;
+
+    public Contacto(String nombre, String telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Contacto{" +
+                "nombre='" + nombre + '\'' +
+                ", telefono='" + telefono + '\'' +
+                '}';
     }
 }
