229f675805770d61cdfca675b84554cda48a8087
sevenban46 <oscarcanizales2000@gmail.com>
2026-04-06 00:47:00 -0600

    mostrar metodos

--- ejercicios-java/src/main/java/com/alexc/ejercicios/week04/Pagable.java
+++ ejercicios-java/src/main/java/com/alexc/ejercicios/week04/Pagable.java
@@ -3,4 +3,7 @@ package com.alexc.ejercicios.week04;
 public interface Pagable {
 
     // TODO: agregar un metodo como calcularPago o mostrarResumen.
+    double calcularPago();
+    void mostrarResumen();
+
 }
