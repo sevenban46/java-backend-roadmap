c12709ed42cd03e7aacd29e750db04f28740cc1c
sevenban46 <oscarcanizales2000@gmail.com>
2026-04-06 01:44:04 -0600

    finalizando

--- ejercicios-java/src/main/java/com/alexc/ejercicios/week06/EntradaSeguraTryCatch.java
+++ ejercicios-java/src/main/java/com/alexc/ejercicios/week06/EntradaSeguraTryCatch.java
@@ -1,8 +1,24 @@
 package com.alexc.ejercicios.week06;
 
+import java.util.Scanner;
 public class EntradaSeguraTryCatch {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         // TODO: capturar una entrada invalida sin cerrar el programa.
+
+        while (true) {
+            System.out.print("Ingrese un numero entero: ");
+            try {
+                int numero = scanner.nextInt();
+                System.out.println("El numero ingresado es: " + numero);
+                break; // Salir del bucle si la entrada es válida
+            } catch (Exception e) {
+                System.out.println("Entrada invalida. Por favor, ingrese un numero entero.");
+                scanner.nextLine(); // Limpiar el buffer de entrada
+            }
+        }
+
+         scanner.close(); // Cerrar el scanner después de cada intento
     }
 }
