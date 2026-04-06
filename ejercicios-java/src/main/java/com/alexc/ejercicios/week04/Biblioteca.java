c4c5417f4ebfa35473fad51a1a8497abd36e4091
sevenban46 <oscarcanizales2000@gmail.com>
2026-04-06 00:55:26 -0600

    biblioteca

--- ejercicios-java/src/main/java/com/alexc/ejercicios/week04/Biblioteca.java
+++ ejercicios-java/src/main/java/com/alexc/ejercicios/week04/Biblioteca.java
@@ -1,6 +1,27 @@
 package com.alexc.ejercicios.week04;
 
+import java.util.Arrays;
+
 public class Biblioteca {
 
     // TODO: usar composicion para almacenar varios libros.
+    private Libro[] libros;
+    private int contador;
+
+    public Biblioteca() {
+        this.libros = new Libro[100]; // Asumimos un máximo de 100 libros
+        this.contador = 0;
+    }
+
+    public void agregarLibro(Libro libro) {
+        if (contador < libros.length) {
+            libros[contador] = libro;
+            contador++;
+        }
+    }
+
+    public Libro[] getLibros() {
+        return Arrays.copyOf(libros, contador);
+    }
+
 }
