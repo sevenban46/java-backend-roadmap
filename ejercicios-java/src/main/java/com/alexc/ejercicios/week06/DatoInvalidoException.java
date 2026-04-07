62af5929d0226a732277a7cb7560607e61446cd5
sevenban46 <oscarcanizales2000@gmail.com>
2026-04-07 00:17:11 -0600

    cubrimos la semana 6

--- ejercicios-java/src/main/java/com/alexc/ejercicios/week06/DatoInvalidoException.java
+++ ejercicios-java/src/main/java/com/alexc/ejercicios/week06/DatoInvalidoException.java
@@ -5,4 +5,8 @@ public class DatoInvalidoException extends Exception {
     public DatoInvalidoException(String message) {
         super(message);
     }
+
+    public DatoInvalidoException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
--- ejercicios-java/src/main/java/com/alexc/ejercicios/week06/Nota.java
+++ ejercicios-java/src/main/java/com/alexc/ejercicios/week06/Nota.java
@@ -3,4 +3,30 @@ package com.alexc.ejercicios.week06;
 public class Nota {
 
     // TODO: agregar atributos privados como nombreMateria y valor.
+    private String nombreMateria;
+    private double valor;
+
+    // TODO: agregar constructor
+    public Nota(String nombreMateria, double valor) {
+        this.nombreMateria = nombreMateria;
+        this.valor = valor;
+    }
+
+    // TODO: agregar métodos getter y setter
+    public String getNombreMateria() {
+        return nombreMateria;
+    }
+
+    public void setNombreMateria(String nombreMateria) {
+        this.nombreMateria = nombreMateria;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
 }
