a7aae7ebf22fcf6f7d6d5b244047239c5b109e2f
sevenban46 <oscarcanizales2000@gmail.com>
2026-04-07 00:02:17 -0600

    guardando rchivo

--- ejercicios-java/src/main/java/com/alexc/ejercicios/week06/GuardarTextoEnArchivo.java
+++ ejercicios-java/src/main/java/com/alexc/ejercicios/week06/GuardarTextoEnArchivo.java
@@ -1,8 +1,23 @@
 package com.alexc.ejercicios.week06;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 public class GuardarTextoEnArchivo {
 
     public static void main(String[] args) {
         // TODO: guardar datos simples en un archivo de texto.
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el texto que desea guardar en el archivo: ");
+        String texto = scanner.nextLine();
+        scanner.close();
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("texto_guardado.txt"))) {
+            writer.write(texto);
+            System.out.println("Texto guardado en el archivo.");
+        } catch (IOException e) {
+            System.out.println("Error al guardar el texto en el archivo.");
+        }
     }
 }
